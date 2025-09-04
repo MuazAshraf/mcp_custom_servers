4b355e96e7761f99ad8854b4dbfac0cbe68863a5
MuazAshraf <muazashraf456@gmail.com>
2025-09-04 16:51:46 +0500

    add new mcp server core-services

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from github_server import mcp as github_mcp
 from prd_server import mcp as prd_mcp
 from vimeo_server import mcp as vimeo_mcp
 from mailgun_server import mcp as mailgun_mcp
-from dashboard_server import mcp as dashboard
+from dashboard_server import mcp as dashboard_mcp
 
 import os
 import contextlib
@@ -19,7 +19,7 @@ async def lifespan(app: FastAPI):
         await stack.enter_async_context(prd_mcp.session_manager.run())
         await stack.enter_async_context(vimeo_mcp.session_manager.run())
         await stack.enter_async_context(mailgun_mcp.session_manager.run())
-        await stack.enter_async_context(dashboard.session_manager.run())
+        await stack.enter_async_context(dashboard_mcp.session_manager.run())
         yield
 
 # Create FastAPI app with lifespan
@@ -36,7 +36,7 @@ app.mount("/github", github_mcp.streamable_http_app())
 app.mount("/prd", prd_mcp.streamable_http_app())
 app.mount("/vimeo", vimeo_mcp.streamable_http_app())
 app.mount("/mailgun", mailgun_mcp.streamable_http_app())
-app.mount("/dashboard", dashboard.streamable_http_app())
+app.mount("/dashboard", dashboard_mcp.streamable_http_app())
 
 
 PORT = int(os.environ.get("PORT", 8000))
